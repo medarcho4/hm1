04dface596be87dc9adf90cec29dea9b479e5671
medarcho4 <adammazhidov7@gmail.com>
2024-07-06 16:46:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+
+#2nd programm
+print(9.99>9.98 and 1000!=1000.1)
+
+#3rd programm
+number1=1234
+number2=5678
+print((number1//53)+(number2%5611))
+
+#4th program
+n1=13.42
+n2=42.13
+me1=n1//1
+me2=n2//1
+me3=n1%13
+me4=n2%42
+print((me3+(0.42-me3))*100==me2 or me4*100-256*10**(-15))
