ff6a215963941c46acfd8bd86104250e99456987
medarcho4 <adammazhidov7@gmail.com>
2024-07-09 18:05:33 +0300

    Add files via upload

---
+++ hm1.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+
+#2nd programm
+print(9.99>9.98 and 1000!=1000.1)
+
+#3rd programm
+number1=1234
+number2=5678
+print((number1//10%100)+(number2//10%100))
+
+#4th program
+n1=13.42
+n2=42.13
+me1=n1//1
+me2=n2//1
+me3=n1*100%100
+me4=n2*100%100
+print(me1==me4 or me2==me3)
